3c57f7239dbe598b611f6965286cbdebbcfb81f3
j340m3 <bergmann.jerome@gmail.com>
2016-09-16 17:09:49 +0200

    this should be private ahem

--- pycode/utils.py
+++ pycode/utils.py
@@ -1,28 +1,52 @@
 class Decorator:
     def __new__(self,*args,**kwargs):
-        print("new", args, kwargs)
         self.decoree = None
         self.newargs = args
         self.newkwargs = kwargs
-        if args and callable(args[0]):
-            if len(args) == 1 and len(kwargs) == 0:
-                return args[0]
-        elif args and isinstance(args[0],type):
-            self.decoree = args[0]
+        if args:
+            if isinstance(args[0],type):
+                self.decoree = args[0]
+            if callable(args[0]):
+                if len(args) == 1 and len(kwargs) == 0:
+                    return args[0]
+            else:
+                pass
         return self
 
     def __init__(self,*args,**kwargs):
-        print ("init",args,kwargs)
         self.initargs = args
         self.initkwargs = kwargs
         if args and callable(args[0]):
             self.decoree = args[0]
 
     def __call__(self, *args, **kwargs):
-        print ("call",args,kwargs)
         if args and callable(args[0]):
             self.decoree = args[0]
+        if not callable(self.decoree):
             return self.decoree
         elif self.decoree:
             return self.decoree(*args,**kwargs)
+
+    @staticmethod
+    def create_wrapping_class(cls):
+        from future.utils import with_metaclass
+        class MetaNewClass(type):
+            def __repr__(self):
+                return repr(cls)
+
+        class NewClass(with_metaclass(MetaNewClass,cls)):
+            def __init__(self,*args,**kwargs):
+                self.__instance = cls(*args,**kwargs)
+            "This is the overwritten class"
+            def __getattribute__(self, attr_name):
+                if attr_name == "__class__":
+                    return cls
+                obj = super(NewClass, self).__getattribute__(attr_name)
+                if hasattr(obj, '__call__'):
+                    return obj
+                return obj
+            def __repr__(self):
+                return repr(self.__instance)
+        return NewClass
+
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -149,5 +149,33 @@ class TestDecorator(unittest.TestCase):
         self.assertEqual(a_inst.__class__.__name__, "A")
         self.assertEqual(a_inst.a(), 2)
 
+    def test_class_as_only_argument(self):
+        class B:
+            def __init__(self, *args, **kwargs):
+                self.args = args
+                self.kwargs = kwargs
+
+        @Decorator(B,4)
+        class A:
+            def a(self):
+                return 2
+        a_inst = A()
+
+        self.assertIsNotNone(a_inst)
+        self.assertEqual(a_inst.__class__.__name__,"A")
+        self.assertEqual(a_inst.a(),2)
+
+    def test_class_decorator(self):
+
+        class A:
+            def __str__(self):
+                return "Bla"
+        a = Decorator.create_wrapping_class(A)
+        self.assertEqual(str(a()),"Bla")
+        self.assertEqual(a().__class__.__name__,"A")
+        self.assertEqual(repr(a()).split("at 0x")[0],repr(A()).split("at 0x")[0])
+        self.assertIsInstance(a(),A)
+        self.assertEqual(repr(a),repr(A))
+
 if __name__ == "__main__":
     unittest.main()
